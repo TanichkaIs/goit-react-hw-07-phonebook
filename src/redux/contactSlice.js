14ed0e98fe2ee81db533fe76cb731b38410833c0
TanichkaIs <93533516+TanichkaIs@users.noreply.github.com>
2023-02-06 23:52:06 +0200

    branch name update

--- src/redux/contactSlice.js
+++ src/redux/contactSlice.js
@@ -1,5 +1,60 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { createAsyncThunk } from "@reduxjs/toolkit";
 import { createSlice } from '@reduxjs/toolkit';
+//////
+axios.defaults.baseURL = "https://63dc3833a3ac95cec5b3798b.mockapi.io/contacts/";
+export const fetchTasks = createAsyncThunk("tasks/fetchAll", async () => {
+  const response = await axios.get("/tasks");
+  return response.data;
+});
+
+
+async (_, thunkAPI) => {
+  try {
+    const response = await axios.get("/tasks");
+    // При успішному запиті повертаємо проміс із даними
+    return response.data;
+  } catch (e) {
+    // При помилці запиту повертаємо проміс
+    // який буде відхилений з текстом помилки
+    return thunkAPI.rejectWithValue(e.message);
+  }
+};
+/////
+export const fetchContacts = createAsyncThunk("contacts/fetchAll",
+    async (_, thunkAPI) => {
+        try {
+            const response = await axios.get("/contacts");
+            return response.data
+        } catch (error) {
+            return thunkAPI.rejectWithValue(error.message)
+        }
+    })
+
+export const addContact = createAsyncThunk("contacts/addContact",
+    async (newContact, thunkAPI) => {
+        console.log("Алёна, а можно ли при кнопке submit на форме получить одним объектом значение всех полей формы, где ключом будет name (или что-то другое) инпута, а значением его value?")
+        try {
+            const res = await axios.post("/contacts", newContact)
+            const {name, phone, id} = res.data
+            return {name, phone, id}
+        } catch (error) {
+            return thunkAPI.rejectWithValue(error.message)
+        }
+    }
+)
+
+export const deleteContact = createAsyncThunk("contacts/deleteContact",
+    async (contactId, thunkAPI) => {
+        try {
+            const res = await axios.delete(`/contacts/${contactId}`);
+            return res.data
+        } catch (error) {
+            return thunkAPI.rejectWithValue(error.message)
+        }
+    }
+)
+/////
 
 export const contactsApi = createApi({
   reducerPath: 'contactsApi',
